4965bf3e2b5e95d721a1dabd2695c13ddfc5a2e0
Петр Кравцов <rat.f.br@gmail.com>
2022-06-08 17:27:07 +0300

    hand test, new idea

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-# if __name__ == '__main__':
-#     pass
+import csv
+
+
+def csv_to_list(file_path: str) -> list:
+    """
+    Читает файл с исходными данными и преобразует в список кортежей для дальнейшей обработки
+    """
+    res_list = []
+    with open(file_path)  as csvfile:
+        intro_data = csv.reader(csvfile)
+        for row in intro_data:
+            res_list.append(tuple(row))
+    return res_list
+
+
+def create_weighted_destinations(ways_list: list) -> dict:
+    """
+    Создаёт из исходного списка симметричный словарь со взвешенными расстояниями до соседних пунктов
+    """
+    res_dict = {}
+    for way in ways_list:
+        if res_dict.get(way[0]):
+            res_dict[way[0]][way[1]] = way[2] * way[3]
+        else:
+            res_dict[way[0]] = {way[1]: way[2] * way[3]}
+    return res_dict
+
+if __name__ == '__main__':
+    data = csv_to_list('test_data.csv')
+    print(data)
